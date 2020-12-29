1e55f14052c36209757b37b952769f5c7b91574c
qknamskaksnna <76665470+qknamskaksnna@users.noreply.github.com>
2020-12-29 17:42:18 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.Token = ""; //Bot Token - Important
-exports.Owner = ""; //Bot Owner ID - Important
-exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Token = "NzkzNTE5MTg5NTYxNzA0NTE4.X-tcVQ.hGaYG088SZ9OVEKKdJs3eMcyU7o"; //Bot Token - Important
+exports.Owner = "637299944939585576"; //Bot Owner ID - Important
+exports.Default_Prefix = "$p"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "#050505"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
 exports.Support = ``; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
